38176bb6129aa35d740830913927a4460bb81c28
decolongon <jcsa.decolongon.ui@phinmaed.com>
2026-03-12 10:32:10 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ const urlsToCache = [
   "./icon-512.png"
 ];
 
-// Install event: cache files
+
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -16,7 +16,7 @@ self.addEventListener("install", event => {
   );
 });
 
-// Activate event: clean old caches
+
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -31,7 +31,7 @@ self.addEventListener("activate", event => {
   );
 });
 
-// Fetch event: serve cached files if available
+
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
@@ -40,3 +40,4 @@ self.addEventListener("fetch", event => {
   );
 
 });
+
